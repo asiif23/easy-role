cc81b8083df4e5e952b5a43cb8e9e3f1fcc86674
asiifdev <asiifdev@users.noreply.github.com>
2023-02-27 02:19:12 +0000

    Fix styling

--- src/Commands/BuatRole.php
+++ src/Commands/BuatRole.php
@@ -2,9 +2,9 @@
 
 namespace Asiifdev\EasyRole\Commands;
 
-use Illuminate\Console\Command;
 use Asiifdev\EasyRole\Models\Role;
 use Carbon\Carbon;
+use Illuminate\Console\Command;
 
 class BuatRole extends Command
 {
@@ -34,23 +34,23 @@ class BuatRole extends Command
         $progressBar->setMessage('Meghitung Total Role yang sudah ada..');
         $progressBar->start();
         $progressBar->setBarCharacter('*');
-        foreach($roles as $customer) {
+        foreach ($roles as $customer) {
             //Do whatever processing that is required per customer
             $progressBar->advance();
         }
         $progressBar->finish();
         $this->newLine();
         $cek = Role::where('name', $this->argument('nama'))->get();
-        $this->alert('Total Role yang sudah ada ' . count($roles));
+        $this->alert('Total Role yang sudah ada '.count($roles));
         $this->table(
             ['Nama role', 'dibuat pada'],
             Role::all(['name', 'created_at'])->toArray(), 'default'
         );
-        if(count($cek) > 0){
-            $this->error('Role dengan nama ' . $this->argument('nama') . ' Sudah ada!');
+        if (count($cek) > 0) {
+            $this->error('Role dengan nama '.$this->argument('nama').' Sudah ada!');
+
             return;
-        }
-        else{
+        } else {
             Role::create([
                 'name' => $this->argument('nama'),
                 'guard_name' => 'web',
@@ -58,11 +58,11 @@ class BuatRole extends Command
             // if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
             //     system('cls');
             // } else {
-                // }
+            // }
             system('clear');
             system('cls');
             $created = Carbon::now()->format('Y-m-d H:i:s');
-            $this->alert('Role dengan nama ' . $this->argument('nama') . ' Berhasil dibuat pada ' . $created);
+            $this->alert('Role dengan nama '.$this->argument('nama').' Berhasil dibuat pada '.$created);
             $this->table(
                 ['Nama role', 'dibuat pada'],
                 Role::all(['name', 'created_at'])->toArray(), 'default'
